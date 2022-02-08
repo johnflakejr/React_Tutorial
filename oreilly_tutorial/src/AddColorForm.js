9b281692ce8562dd6fce0719ecbe9c8fa9572232
John Lake <john.f.lake5.mil@mail.mil>
2022-02-08 13:55:06 -0500

    Done with chapter 6

--- oreilly_tutorial/src/AddColorForm.js
+++ oreilly_tutorial/src/AddColorForm.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { useInput } from './hooks';
+import { useColors } from './color-hooks';
 
 
-export default function AddColorForm({onNewColor = f=>f}) {
+export default function AddColorForm() {
   const [titleProps, resetTitle] = useInput("");
   const [colorProps, resetColor] = useInput("#000000");
 
+  const { addColor } = useColors();
+
   const submit = e => {
     e.preventDefault();
-    onNewColor(titleProps.value, colorProps.value);
+    addColor(titleProps.value, colorProps.value);
     resetTitle();
     resetColor()
   };
--- oreilly_tutorial/src/ColorList.js
+++ oreilly_tutorial/src/ColorList.js
@@ -1,19 +1,20 @@
 import React from "react";
 import Color from "./Color";
+import { useColors } from "./color-hooks";
 
-export default function ColorList({colors = [], onRemoveColor = f => f, onRateColor = f => f}) {
+export default function ColorList() {
+  const { colors } = useColors();
+
   if (colors.length < 1) {
     return <div>No Colors Listed.</div>;
   }
 
   return (
-    <div>
+    <div className="color-list">
       {
-        colors.map(color => <Color key={color.id} {...color} onRemove={onRemoveColor} onRate={onRateColor}/>)
+        colors.map(color => <Color key={color.id} {...color}/>)
       }
     </div>
-
-
   );
 
 }
